21aa938efd5c90095e98062e1a99cc82fcf151b1
Sidi Mohamed <bettars@findlay.edu>
2024-07-11 04:52:02 -0400

    diname

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ import ticketRouter from './routes/ticketRoutes.js';
 import userRouter from './routes/userRoute.js';
 
 dotenv.config();
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname = path.dirname(__filename); 
 
 const app = express();
 
