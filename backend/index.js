35286d480c257e03699ea063ec96207df5db07f2
Sidi Mohamed <bettars@findlay.edu>
2024-07-11 03:23:22 -0400

    fixing bugs

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,18 @@ import dotenv from 'dotenv';
 import cors from 'cors'
 import  ticketRouter from './routes/ticketRoutes.js'
 import userRouter from './routes/userRoute.js'
+import path from 'path'; // Provides utilities for working with file and directory paths
+import { fileURLToPath } from 'url'; // Converts a file URL to a file path
 dotenv.config()
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(cors())
+
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname = path.dirname(__filename); 
+app.use(express.static(path.join(__dirname, '../frontend/dist'))); // Serve static files
 app.use(
     cors({
         origin: "*",
@@ -31,3 +37,4 @@ mongoose.connect(process.env.MONGO)
 
 app.use('/api/ticket', ticketRouter)
 app.use('/api/user', userRouter)
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../frontend/dist/index.html')));
